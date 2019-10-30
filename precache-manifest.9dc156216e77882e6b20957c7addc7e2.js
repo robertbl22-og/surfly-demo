4038b511f75ab474123454e975d83098ab84c739
Robert Blaske <rblaske@onpointglobal.com>
2019-10-30 16:23:28 -0400

    Updates

---
+++ precache-manifest.9dc156216e77882e6b20957c7addc7e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace3e9cc5954cca7e89f5489c982081d",
+    "url": "/surfly-demo/index.html"
+  },
+  {
+    "revision": "ab9d9ef7f90afa240a12",
+    "url": "/surfly-demo/static/css/main.ebfe8ec0.chunk.css"
+  },
+  {
+    "revision": "b4afadec59dd27997bc7",
+    "url": "/surfly-demo/static/js/2.d54ac364.chunk.js"
+  },
+  {
+    "revision": "ab9d9ef7f90afa240a12",
+    "url": "/surfly-demo/static/js/main.741e26f5.chunk.js"
+  },
+  {
+    "revision": "d90953264df767ec3d36",
+    "url": "/surfly-demo/static/js/runtime-main.d820d35a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/surfly-demo/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.f395bfd283b86f23c2628adf17443df8.js"
+  "/surfly-demo/precache-manifest.9dc156216e77882e6b20957c7addc7e2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/surfly-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
